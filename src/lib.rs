838f188df78263437a9ddabc70ef708a4f9dd96b
纳西妲 · Nahida <me@nahida.im>
2025-02-02 22:51:35 +0800

    perf(lib.rs): change the position of `power-by-akasha-subscriber`

--- src/lib.rs
+++ src/lib.rs
@@ -44,12 +44,15 @@ async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
             .and_then(|flag| flag.as_bool())
             .is_none_or(|flag| flag)
         {
-            proxies.push(yaml::from_str(
-                r#"name: "此订阅由github.com/Buer-Nahida/akasha-subscriber虚空订阅器生成"
+            proxies.insert(
+                0,
+                yaml::from_str(
+                    r#"name: "此订阅由github.com/Buer-Nahida/akasha-subscriber虚空订阅器生成"
 server: nahida.im
 type: hysteria2
 port: 443"#,
-            )?);
+                )?,
+            );
         }
         let proxies = proxies.try_into_clash_style_proxies(&user, &query).await?;
 
